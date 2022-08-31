7b813bfddb6c90c1e9ac4156f74efa476cb6fd05
loctk <loctk@vmodev.com>
2022-08-31 17:05:35 +0700

    Init ssr

---
+++ src/client/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Home 1234
+    <div><button onClick={() => {console.log('test')}}>Test</button></div>
+  </div>;
+};
+
+export default Home;
